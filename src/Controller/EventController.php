11cce440479d4baef072ec7d44f311761cd982f8
Benjamin Zaslavsky <benjamin.zaslavsky@gmail.com>
2024-11-06 10:52:17 +0100

    List & show event templates

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -6,24 +6,29 @@ use App\Entity\Event;
 use App\Repository\EventRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class EventController extends AbstractController
 {
-    #[Route('/event', name: 'app_event_listevent')]
+    #[Route('/event', name: 'app_event_listevent', methods: ['GET'])]
     public function listEvents(EventRepository $repository): Response
     {
-        $events = $repository->findAll();
-        $list = \array_map(
-            fn(Event $e) => ['id' => $e->getId(), 'name' => $e->getName()],
-            $events
-        );
-
-        return $this->json($list);
+        return $this->render('event/list_events.html.twig', [
+            'events' => $repository->findAll(),
+        ]);
     }
 
-    #[Route('/event/{name}/{start}/{end}', name: 'app_event_new', requirements: ['start' => '[0-9-]{10}', 'end' => '[0-9-]{10}'])]
+    #[Route('/event/{id}', name: 'app_event_showevent', requirements: ['id' => '\d+'], methods: ['GET'])]
+    public function showEvent(?Event $event): Response
+    {
+        return $this->render('event/show_event.html.twig', [
+            'event' => $event,
+        ]);
+    }
+
+    #[Route('/event/{name}/{start}/{end}', name: 'app_event_new', requirements: ['start' => '[0-9-]{10}', 'end' => '[0-9-]{10}'], methods: ['GET'])]
     public function newEvent(string $name, string $start, string $end, EntityManagerInterface $manager): Response
     {
         $event = (new Event())
